406d33d3f66c1a2081497728681578ec78e4c159
Sharon-B <sharonbagnall@gmail.com>
2020-12-02 19:17:50 +0000

    Callback Function 1

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+function getData(cb) {
+    let xhr = new XMLHttpRequest();
+
+
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+
+
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+getData(function(data) {
+    console.log(data);
+});
+
+
+
+
+/*
 let xhr = new XMLHttpRequest();
 let data;
 
